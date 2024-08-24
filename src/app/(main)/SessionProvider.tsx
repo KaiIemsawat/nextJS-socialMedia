588182776b9e56babac085ff828a3114c1543ca4
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-24 09:55:03 -0400

    Context provider custom hook, relocated page.tsx, fix schema.prisma

--- src/app/(main)/SessionProvider.tsx
+++ src/app/(main)/SessionProvider.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { Session, User } from "lucia";
-import React, { createContext } from "react";
+import React, { createContext, useContext } from "react";
 
 interface SessionContext {
   user: User;
   session: Session;
 }
 
+// This is nullable
 const SessionContext = createContext<SessionContext | null>(null);
 
 export default function SessionProvider({
@@ -17,4 +18,13 @@ export default function SessionProvider({
   return (
     <SessionContext.Provider value={value}>{children}</SessionContext.Provider>
   );
+}
+
+// Make SessionContext not nullable
+export function useSession() {
+  const context = useContext(SessionContext);
+  if (!context) {
+    throw new Error("useSession must be used within a SessionProvider");
+  }
+  return context;
 }
