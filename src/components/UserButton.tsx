7ad76e82b5c29cb56808cada959465ad5f42507e
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-24 20:39:36 -0400

    navbar - part 2 - dropdown

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import { useSession } from "@/app/(main)/SessionProvider";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 import UserAvatar from "./UserAvatar";
+import Link from "next/link";
+import { LogOutIcon, UserIcon } from "lucide-react";
+import { logout } from "@/app/(auth)/actions";
+import { cn } from "@/lib/utils";
 
 interface UserButtonProps {
   className?: string;
@@ -14,10 +25,36 @@ export default function UserButton({ className }: UserButtonProps) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <button className="">
+        {/* AVATAR */}
+        <button className={cn("flex-none rounded-full", className)}>
           <UserAvatar avatarUrl={user.avatarUrl} size={40} />
         </button>
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        {/* TOP - LOGGED IN AS */}
+        <DropdownMenuLabel>Logged in as @{user.username}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+
+        {/* PROFILE */}
+        <Link href={`/user.username`}>
+          <DropdownMenuItem>
+            <UserIcon className="mr-2 size-4" />
+            Profile
+          </DropdownMenuItem>
+        </Link>
+        <DropdownMenuSeparator />
+
+        {/* LOGOUT */}
+        <DropdownMenuItem
+          onClick={() => {
+            logout();
+          }}
+        >
+          <LogOutIcon className="mr-2 size-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 }
