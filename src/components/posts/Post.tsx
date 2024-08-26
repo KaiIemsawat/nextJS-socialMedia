e6f58bf04e0c1969dc5e4c11cf4245d5c6adab29
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-26 09:23:31 -0400

    refactored import PostData, base render Post.tsx

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -1,9 +1,31 @@
-import { Post as PostData } from "@prisma/client";
+import Link from "next/link";
+import { PostData } from "../ui/type";
+import UserAvatar from "../UserAvatar";
 
 interface PostProps {
   post: PostData;
 }
 
 export default function Post({ post }: PostProps) {
-  return <article>{post.content}</article>;
+  return (
+    <article className="space-y-3 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="flx flex-wrap gap-3">
+        <Link href={`/users/${post.user.username}`}>
+          <UserAvatar avatarUrl={post.user.avatarUrl} />
+        </Link>
+        <div>
+          <Link
+            href={`/users/${post.user.username}`}
+            className="block font-medium hover:underline"
+          >
+            {post.user.displayName}
+          </Link>
+          <Link
+            href={`/posts/${post.id}`}
+            className="block text-sm text-muted-foreground hover:underline"
+          ></Link>
+        </div>
+      </div>
+    </article>
+  );
 }
