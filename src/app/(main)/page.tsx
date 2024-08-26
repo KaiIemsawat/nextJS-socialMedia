716d04c27575f336e470771d3984e5e10b13b324
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-26 09:07:45 -0400

    render posts - base

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,28 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    orderBy: { createdAt: "desc" },
+    // Select what to fetch
+    include: {
+      user: {
+        select: {
+          username: true,
+          displayName: true,
+          avatarUrl: true,
+        },
+      },
+    },
+  });
   return (
     <main className="h-[200vh] w-full bg-red-50">
       <div className="w-full">
         <PostEditor />
+        {posts.map((post) => (
+          <Post key={post.id} post={post} />
+        ))}
       </div>
     </main>
   );
