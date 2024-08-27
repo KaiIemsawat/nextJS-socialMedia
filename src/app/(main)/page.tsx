c9e393c1564b90f87cd9cdc83729ca2e40d86243
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-27 10:32:20 -0400

    ForYouFeed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,20 +3,14 @@ import Post from "@/components/posts/Post";
 import TrendsSidebar from "@/components/TrendsSidebar";
 import { postDataInclude } from "@/lib/type";
 import prisma from "@/lib/prisma";
+import ForYouFeed from "./ForYouFeed";
 
-export default async function Home() {
-  const posts = await prisma.post.findMany({
-    orderBy: { createdAt: "desc" },
-    // Select what to fetch
-    include: postDataInclude,
-  });
+export default function Home() {
   return (
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
+        <ForYouFeed />
       </div>
       <TrendsSidebar />
     </main>
