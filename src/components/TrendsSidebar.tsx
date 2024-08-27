9d17e011f0067b86108455820f40450016eadc57
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-26 21:35:40 -0400

    TrendSidebar render hashtag and number of tags. utils > format numbers

--- src/components/TrendsSidebar.tsx
+++ src/components/TrendsSidebar.tsx
@@ -4,14 +4,18 @@ import { userDataSelect } from "@/lib/type";
 import { Loader2 } from "lucide-react";
 import Link from "next/link";
 import { Suspense } from "react";
+import { unstable_cache } from "next/cache";
+
 import UserAvatar from "./UserAvatar";
 import { Button } from "./ui/button";
+import { formatNumber } from "@/lib/utils";
 
 export default function TrendsSidebar() {
   return (
     <div className="sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80">
       <Suspense fallback={<Loader2 className="mx-auto animate-spin" />}>
         <WhoToFollow />
+        <TrendingTopics />
       </Suspense>
     </div>
   );
@@ -54,6 +58,54 @@ async function WhoToFollow() {
           <Button>Follow</Button>
         </div>
       ))}
+    </div>
+  );
+}
+
+const getTrendingTopics = unstable_cache(
+  async () => {
+    const result = await prisma.$queryRaw<{ hashtag: string; count: bigint }[]>`
+            SELECT LOWER(unnest(regexp_matches(content, '#[[:alnum:]_]+', 'g'))) AS hashtag, COUNT(*) AS count
+            FROM posts
+            GROUP BY (hashtag)
+            ORDER BY count DESC, hashtag ASC
+            LIMIT 5
+        `;
+
+    return result.map((row) => ({
+      hashtag: row.hashtag,
+      count: Number(row.count),
+    }));
+  },
+  ["trending_topics"],
+  {
+    revalidate: 3 * 60 * 60,
+  },
+);
+
+async function TrendingTopics() {
+  const trendingTopics = await getTrendingTopics();
+
+  return (
+    <div className="space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <div className="text-xl font-bold">Trending topics</div>
+      {trendingTopics.map(({ hashtag, count }) => {
+        const title = hashtag.split("#")[1];
+
+        return (
+          <Link key={title} href={`/hashtah/${title}`} className="block">
+            <p
+              className="line-clamp-1 break-all font-semibold hover:underline"
+              title={hashtag}
+            >
+              {hashtag}
+            </p>
+            <p className="text-sm text-muted-foreground">
+              {formatNumber(count)} {count === 1 ? "post" : "posts"}
+            </p>
+          </Link>
+        );
+      })}
     </div>
   );
 }
