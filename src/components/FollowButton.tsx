f3809fc98b153cfff1052f8e7b9735373df42b4a
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-29 18:16:57 -0400

    follow/unfollow backend logic

---
+++ src/components/FollowButton.tsx
@@ -0,0 +1,38 @@
+import useFollowerInfo from "@/hooks/useFollowerInfo";
+import { FollowerInfo } from "@/lib/type";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useToast } from "./ui/use-toast";
+import { Button } from "./ui/button";
+import kyInstance from "@/lib/ky";
+
+interface FollowButtonProps {
+  userId: string;
+  initialState: FollowerInfo;
+}
+
+export default function FollowButton({
+  userId,
+  initialState,
+}: FollowButtonProps) {
+  const { toast } = useToast();
+
+  const queryClient = useQueryClient();
+
+  const { data } = useFollowerInfo(userId, initialState);
+
+  const { mutate } = useMutation({
+    mutationFn: () =>
+      data.isFollowedByUser
+        ? kyInstance.delete(`/api/users/${userId}/followers`)
+        : kyInstance.post(`/api/users/${userId}/followers`),
+  });
+
+  return (
+    <Button
+      variant={data.isFollowedByUser ? "secondary" : "default"}
+      onClick={() => mutate()}
+    >
+      {data.isFollowedByUser ? "Unfollow" : "Follow"}
+    </Button>
+  );
+}
