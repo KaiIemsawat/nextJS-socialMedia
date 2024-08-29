b462e31c832473157f663a0035d883c413b3dacd
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-28 21:54:34 -0400

    more button UI

---
+++ src/components/posts/PostMoreButton.tsx
@@ -0,0 +1,35 @@
+import { PostData } from "@/lib/type";
+import { useState } from "react";
+import DeletePostDialog from "./DeletePostDialog";
+import { DropdownMenu, DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { MoreHorizontalIcon } from "lucide-react";
+
+interface PostMoreButtonProps {
+  post: PostData;
+  className?: string;
+}
+
+export default function PostMoreButton({
+  post,
+  className,
+}: PostMoreButtonProps) {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+
+  return (
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="icon" variant="ghost" className={className}>
+            <MoreHorizontalIcon className="size-5 text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+      </DropdownMenu>
+      <DeletePostDialog
+        post={post}
+        open={showDeleteDialog}
+        onClose={() => setShowDeleteDialog(false)}
+      />
+    </>
+  );
+}
