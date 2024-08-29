22a6bea4c24492a00ce6db9b882e43289062ed33
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-29 15:16:20 -0400

    POST request - request following

--- src/app/api/users/[userId]/followers.route.ts
+++ src/app/api/users/[userId]/followers.route.ts
@@ -48,3 +48,36 @@ export async function GET(
     return Response.json({ error: "Internal server error" }, { status: 500 });
   }
 }
+
+export async function POST(
+  req: Request,
+  { params: { userId } }: { params: { userId: string } },
+) {
+  try {
+    const { user: loggedInUser } = await validateRequest();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.follow.upsert({
+      where: {
+        followerId_followingId: {
+          // create by prisma from @@unique([followerId, followingId])
+          followerId: loggedInUser.id,
+          followingId: userId,
+        },
+      },
+      create: {
+        followerId: loggedInUser.id,
+        followingId: userId,
+      },
+      update: {},
+    });
+
+    return new Response();
+  } catch (error) {
+    console.error(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
