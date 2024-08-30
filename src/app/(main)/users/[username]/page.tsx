4f291113771b65f03d43eaea49391c7b3a08da1b
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-30 08:09:59 -0400

    update type.ts, fix schema, render profile

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,7 +1,8 @@
 import { validateRequest } from "@/auth";
 import TrendsSidebar from "@/components/TrendsSidebar";
+import UserAvatar from "@/components/UserAvatar";
 import prisma from "@/lib/prisma";
-import { getUserDataSelect } from "@/lib/type";
+import { FollowerInfo, getUserDataSelect, UserData } from "@/lib/type";
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { cache } from "react";
@@ -55,8 +56,34 @@ export default async function page({ params: { username } }: PageProps) {
 
   return (
     <main className="flex w-full min-w-0 gap-5">
-      <div className="w-full min-w-0 space-y-5"></div>
+      <div className="w-full min-w-0 space-y-5">
+        <UserProfile user={user} loggedInUserId={loggedInUser.id} />
+      </div>
       <TrendsSidebar />
     </main>
+  );
+}
+
+interface UserProfileProps {
+  user: UserData;
+  loggedInUserId: string;
+}
+
+async function UserProfile({ user, loggedInUserId }: UserProfileProps) {
+  const followerInfo: FollowerInfo = {
+    followers: user._count.followers,
+    isFollowedByUser: user.followers.some(
+      ({ followerId }) => followerId === loggedInUserId,
+    ),
+  };
+
+  return (
+    <div className="h-fit w-full space-y-5 rounded-2xl bg-card p-5 shadow-sm">
+      <UserAvatar
+        avatarUrl={user.avatarUrl}
+        size={250}
+        className="mx-auto size-full max-h-60 max-w-60 rounded-full"
+      />
+    </div>
   );
 }
