ce251097cc708442b25fa1e9aae2bca764e47d63
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-08-30 07:54:38 -0400

    user profile page base setup

---
+++ src/app/(main)/users/[username]/page.tsx
@@ -0,0 +1,62 @@
+import { validateRequest } from "@/auth";
+import TrendsSidebar from "@/components/TrendsSidebar";
+import prisma from "@/lib/prisma";
+import { getUserDataSelect } from "@/lib/type";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { cache } from "react";
+
+interface PageProps {
+  params: { username: string };
+}
+
+const getUser = cache(async (username: string, loggedInUserId: string) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      username: {
+        equals: username,
+        mode: "insensitive",
+      },
+    },
+    select: getUserDataSelect(loggedInUserId),
+  });
+
+  if (!user) notFound();
+
+  return user;
+});
+
+export async function generateMetadata({
+  params: { username },
+}: PageProps): Promise<Metadata> {
+  const { user: loggedInUser } = await validateRequest();
+
+  if (!loggedInUser) return {};
+
+  const user = await getUser(username, loggedInUser.id);
+
+  return {
+    title: `${user.displayName} (@${user.username})`,
+  };
+}
+
+export default async function page({ params: { username } }: PageProps) {
+  const { user: loggedInUser } = await validateRequest();
+
+  if (!loggedInUser) {
+    return (
+      <p className="text-center text-destructive">
+        You're not authorized to view this page.
+      </p>
+    );
+  }
+
+  const user = await getUser(username, loggedInUser.id);
+
+  return (
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5"></div>
+      <TrendsSidebar />
+    </main>
+  );
+}
