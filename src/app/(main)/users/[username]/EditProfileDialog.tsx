f051d239de94f6797cf50c41ded2dc4ac1f1cba3
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-14 20:37:54 -0400

    display default value on edit profile dialog

--- src/app/(main)/users/[username]/EditProfileDialog.tsx
+++ src/app/(main)/users/[username]/EditProfileDialog.tsx
@@ -5,6 +5,21 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { UserData } from "@/lib/type";
+import {
+  updateUserProfileSchema,
+  UpdateUserProfileValues,
+} from "@/lib/validation";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useUpdateProfileMutation } from "./mutation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface EditProfileDialogProps {
   user: UserData;
@@ -17,12 +32,42 @@ export default function EditProfileDialog({
   open,
   onOpenChange,
 }: EditProfileDialogProps) {
+  const form = useForm<UpdateUserProfileValues>({
+    resolver: zodResolver(updateUserProfileSchema),
+    defaultValues: {
+      displayName: user.displayName,
+      bio: user.bio || "",
+    },
+  });
+
+  const mutation = useUpdateProfileMutation();
+
+  async function onSubmit(values: UpdateUserProfileValues) {
+    // will be implemented
+  }
+
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Edit Profile</DialogTitle>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+            <FormField
+              control={form.control}
+              name="displayName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Display Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Your display name" {...field} />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
