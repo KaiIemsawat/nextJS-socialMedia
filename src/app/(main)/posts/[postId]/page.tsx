a846643d670f1196ae64484cfe6f06bb93fd9597
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-22 17:20:13 -0400

    Post details page part 1. Display post

---
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -0,0 +1,55 @@
+import { validateRequest } from "@/auth";
+import Post from "@/components/posts/Post";
+import prisma from "@/lib/prisma";
+import { getPostDataInclude } from "@/lib/type";
+import { notFound } from "next/navigation";
+import { cache } from "react";
+
+interface PageProps {
+  params: { postId: string };
+}
+
+const getPost = cache(async (postId: string, loggedInUserId: string) => {
+  const post = await prisma.post.findUnique({
+    where: { id: postId },
+    include: getPostDataInclude(loggedInUserId),
+  });
+
+  if (!post) notFound();
+
+  return post;
+});
+
+export async function generateMetadata({ params: { postId } }: PageProps) {
+  const { user } = await validateRequest();
+
+  if (!user) return {};
+
+  const post = await getPost(postId, user.id);
+
+  return {
+    title: `${post.user.displayName}: ${post.content.slice(0, 50)}...`,
+  };
+}
+
+export default async function Page({ params: { postId } }: PageProps) {
+  const { user } = await validateRequest();
+
+  if (!user) {
+    return (
+      <p className="text-destructive">
+        You&apos;re not authorized to view this page.
+      </p>
+    );
+  }
+
+  const post = await getPost(postId, user.id);
+
+  return (
+    <main className="flex w-full min-w-0 gap-5">
+      <div className="w-full min-w-0 space-y-5">
+        <Post post={post} />
+      </div>
+    </main>
+  );
+}
