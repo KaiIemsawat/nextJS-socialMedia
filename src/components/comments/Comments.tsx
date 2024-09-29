85b98c39693028b35b15738eb91cddc39839600f
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-09-28 22:34:35 -0400

    comment box UI

--- src/components/comments/Comments.tsx
+++ src/components/comments/Comments.tsx
@@ -1,9 +1,14 @@
 import { PostData } from "@/lib/type";
+import CommentInput from "./CommentInput";
 
 interface CommentsProps {
   post: PostData;
 }
 
 export default function Comments({ post }: CommentsProps) {
-  return <div>Comment</div>;
+  return (
+    <div>
+      <CommentInput post={post} />
+    </div>
+  );
 }
